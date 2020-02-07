70f25fc7e747b6e29aa283b519f479fa9b6459b9
Rian Apriansyah <jmryan73@gmail.com>
2020-02-07 16:34:47 +0700

    Intial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace codingtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do{
+                Console.WriteLine();
+                Console.WriteLine("================================");
+                Console.WriteLine();
+                Console.Write("Your Input : ");
+                string input = Console.ReadLine();
+                string output = ChequeWriting(input);
+                Console.WriteLine();
+                Console.WriteLine(">> " + output);
+            }
+            while(true);
+        }
+
+        static string ChequeWriting(string input){
+            string output = "";
+            if(!isDigit(input)){
+                output = "Only digit is allowed";
+            }
+            else{
+                output = input;
+            }
+
+            return output;
+        }
+
+        static bool isDigit(string input){
+            string pattern = @"\d";
+            Regex re = new Regex(pattern);
+            return re.IsMatch(input) ?  true : false;
+        }
+    }
+}
